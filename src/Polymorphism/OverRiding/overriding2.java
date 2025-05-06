4a66b57f67bd5ca7fb4238688f814b321fd854e1
Mohdnabee <mohdnabeel989@gamil.com>
2025-05-06 23:33:27 +0530

    06/05/25...

---
+++ src/Polymorphism/OverRiding/overriding2.java
@@ -0,0 +1,30 @@
+package Polymorphism.OverRiding;
+
+  class Shape{
+    void draw(){
+        System.out.println("Drawing a shape...");
+    }
+}
+
+class Circle extends Shape{
+    void draw (){
+        super.draw();
+        System.out.println("Drawing a circle..");
+    }
+}
+
+class square extends Shape{
+    void draw(){
+        System.out.println("Drawing a square..");
+    }
+}
+
+public class overriding2 {
+    public static void main(String[] args) {
+        Shape obj1 = new Circle();
+        Shape obj2 = new square();
+        obj1.draw();
+        obj2.draw();
+
+    }
+}
