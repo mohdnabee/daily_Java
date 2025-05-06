87c64674d9f6d132cddf79be0fbbff834b0af26d
Mohdnabee <mohdnabeel989@gamil.com>
2025-05-06 23:19:51 +0530

    06/05/25...

---
+++ src/Polymorphism/OverRiding/OverRidingMethod.java
@@ -0,0 +1,24 @@
+package Polymorphism.OverRiding;
+
+// method overriding
+class  Vehicle{
+    void run (){
+        System.out.println("run ho  ga code");
+    }
+}
+class Bike extends Vehicle{
+    void run (){
+        System.out.println("New feature add method overriding");
+        super.run();   //super keyword used to call parent class.
+
+    }
+}
+
+public class OverRidingMethod {
+    public static void main(String[] args) {
+
+        Vehicle obj = new Bike();
+        obj.run();
+
+    }
+}
