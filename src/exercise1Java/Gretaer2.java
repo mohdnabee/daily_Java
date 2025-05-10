9d6310dc82adee5f358de049bd2c466bda5d63da
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-10 11:53:36 +0530

    greater of 2 numbers

---
+++ src/exercise1Java/Gretaer2.java
@@ -0,0 +1,34 @@
+package exercise1Java;
+import java.util.*;
+// write a function which takes a 2 numbers and the returns the greater of those two
+public class Gretaer2 {
+
+    //  function
+    public  static  void GreaterOF2(int a , int b){
+        if (a > b) {
+            System.out.println(a + " is greater");
+        } else if (b > a) {
+            System.out.println(b + " is greater");
+        } else {
+            System.out.println("Both numbers are equal");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Scanner scanner= new Scanner(System.in);
+        // Taking input from the user
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        // Call the function with user inputs
+        GreaterOF2(num1, num2);
+
+    }
+}
