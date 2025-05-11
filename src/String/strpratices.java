50a08f182a435fd7cb0c80ff8fbfa1280c64306f
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-11 22:56:38 +0530

    String  Function 11-05-25

---
+++ src/String/strpratices.java
@@ -0,0 +1,14 @@
+package String;
+
+public class strpratices {
+    public static void main(String[] args) {
+        String str =  "HelloWorld";
+
+        System.out.println(str.substring(0,5));
+        System.out.println(str.indexOf("W"));
+        System.out.println(str.charAt(3));
+        System.out.println(str.length());
+
+
+    }
+}
