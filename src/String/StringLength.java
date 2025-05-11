988e92f1837380e3ed4588ded1756c66367e5a0e
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-11 21:19:01 +0530

    String countlegth 11-05-25

---
+++ src/String/StringLength.java
@@ -0,0 +1,20 @@
+package String;
+
+public class StringLength {
+    public static void main(String[] args) {
+
+
+        String name =  "Nabeel";
+        String fullName = "Mohd Nabeel";
+        String senctence = "My name is Mohd Nabeel";
+
+        // count a length of a string==> using a function .length to calculate the words and space
+
+        System.out.println("Name count : "+ name.length());
+        System.out.println("Full Name Count : "+ fullName.length());
+        System.out.println("Sentence count : "+senctence.length());
+
+
+
+    }
+}
