074bdee7c02eee9692edc5ffe2822952691b1a57
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-17 18:21:56 +0530

    sharing

---
+++ src/Sharing/ArithmeticOperators.java
@@ -0,0 +1,33 @@
+package Sharing;
+
+public class ArithmeticOperators {
+
+    public  void  binaryOperators(){
+
+        System.out.println("Arithmetic Operators:-  ");
+        System.out.println(" 1. binary operators \n 2. Unary Operators ");
+        System.out.println();
+        System.out.println("1. binary operators");
+        System.out.println("let A =  10 , B = 5");
+        int a =  10  ,  b = 5 ;
+
+        System.out.println(" + =>  A+B =  "+  a+b );
+        System.out.println(" - =>  A-B =  "+  a+b );
+        System.out.println(" * =>  A*B =  "+  a+b );
+        System.out.println(" / =>  A/B =  "+  a+b );
+        System.out.println(" % =>  A%B =  "+  a+b );
+
+
+        System.out.println();
+        System.out.println("2. Unary Operators");
+        System.out.println(" 1. ++ => Increment Operator \n 2. -- => Decrement Operator");
+
+        System.out.println("1. Increment Operator");
+        System.out.println(" 1. pre Increment Operator = ++A \n 2. Post Increment Operator =  A++");
+        System.out.println();
+        System.out.println("let A =  10 , B = 5");
+
+
+
+    }
+}
---
+++ src/Sharing/OperatorsJava.java
@@ -0,0 +1,10 @@
+package Sharing;
+
+public class OperatorsJava {
+    public static void main(String[] args) {
+
+        ArithmeticOperators  operator1=  new ArithmeticOperators();
+        operator1.binaryOperators();
+
+    }
+}
