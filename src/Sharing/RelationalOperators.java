9d09318ac1c1a63064a17ce3d568df36e669aa6e
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-17 20:49:59 +0530

    Relational Operators

---
+++ src/Sharing/RelationalOperators.java
@@ -0,0 +1,19 @@
+package Sharing;
+
+public class RelationalOperators {
+    public  void RelationOpe() {
+
+        int a = 10; int b =  5 ;
+        System.out.println("Relational Operators");
+        System.out.println("let A =  10  ,  B = 5 ");
+        System.out.println("1.  ==   equal  to operator:  A== B > "+(a==b));
+        System.out.println("2.  !=   not equal  to operator:  A== B > "+(a!=b));
+        System.out.println("3.  >   greater than :  A== B > "+(a>b));
+        System.out.println("4.  <  lesser than operator:  A== B > "+(a<b));
+        System.out.println("5.  >=  greater than equal  to operator:  A== B > "+(a>=b));
+        System.out.println("6.  <=  lesser than equal  to operator:  A== B > "+(a<=b));
+
+
+
+    }
+}
