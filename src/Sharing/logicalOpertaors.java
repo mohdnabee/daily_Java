b8ccb11b1aae9b99b205c015d14f74bf8131113e
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-17 21:40:05 +0530

    logical  Operators

---
+++ src/Sharing/logicalOpertaors.java
@@ -0,0 +1,23 @@
+package Sharing;
+
+public class logicalOpertaors {
+    public  void logicalOpere() {
+
+        System.out.println("LOGICAL OPERATORS");
+        int a = 10;
+        int b = 5;
+        int c = 30;
+
+        System.out.println(" && (logical AND Operator)");
+
+        System.out.println(" || (logical OR Operator)");
+
+
+        System.out.println(" ! (logical NOT  Operator)");
+
+        System.out.println(" Ex : -  ( st1 ||  str2 || str3 ....");
+    }
+
+
+
+}
