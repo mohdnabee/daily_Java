952272bbbe8ac744aea81ab037cf4a75182c9183
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-17 20:30:33 +0530

    Arithmetic Operators

--- src/Sharing/ArithmeticOperators.java
+++ src/Sharing/ArithmeticOperators.java
@@ -27,6 +27,52 @@ public class ArithmeticOperators {
         System.out.println();
         System.out.println("let A =  10 , B = 5");
 
+        System.out.println("1. pre Increment Operator = ++A");
+        System.out.println(" 1.  Change The Value \n 2.  Use Value ");
+        System.out.println( );
+        System.out.println("let A =  10 , B = 0");
+        int aa =  10 ,   bb = 0  ;
+        System.out.println("b = ++a");
+        bb= ++aa;
+        System.out.println("a= "+ aa);
+        System.out.println("b= "+bb);
+
+        System.out.println("2. Post Increment Operator = ++A");
+        System.out.println(" 1.  Use Value  \n 2.  Change The Value");
+        System.out.println( );
+        System.out.println("let A =  10 , B = 0");
+        int aaa =  10 ,   bbb = 0  ;
+        System.out.println("b = a++");
+        bbb= aaa++;
+        System.out.println("a= "+ aaa);
+        System.out.println("b= "+bbb);
+
+
+//  decerement
+        System.out.println("1. decrement Operator");
+        System.out.println(" 1. pre decrement Operator = --A \n 2. Post decrement Operator =  A--");
+        System.out.println();
+        System.out.println("let A =  10 , B = 5");
+
+        System.out.println("1. pre decrement Operator = ++A");
+        System.out.println(" 1.  Change The Value \n 2.  Use Value ");
+        System.out.println( );
+        System.out.println("let A =  10 , B = 0");
+        int aA =  10 ,   bB = 0  ;
+        System.out.println("b = --a");
+        bB= --aA;
+        System.out.println("a= "+ aA);
+        System.out.println("b= "+bB);
+
+        System.out.println("2. Post decrement Operator = ++A");
+        System.out.println(" 1.  Use Value  \n 2.  Change The Value");
+        System.out.println( );
+        System.out.println("let A =  10 , B = 0");
+        int aaA =  10 ,   bbB = 0  ;
+        System.out.println("b = a--");
+        bbB= aaA--;
+        System.out.println("a= "+ aaA);
+        System.out.println("b= "+bbB);
 
 
     }
