af6a1d268e8e4a8dec6f78151e5a3fa1663a9732
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-17 20:14:06 +0530

    bit Manipulation Clear Bit

---
+++ src/bit_Manipulation/bit_ManipulationClearBit.java
@@ -0,0 +1,17 @@
+package bit_Manipulation;
+
+public class bit_ManipulationClearBit {
+    public static void main(String[] args) {
+
+        int  n= 5 ; // 0101
+        int pos =  2;
+        int bitMask  =  1<<pos;
+        int notBitMask =  ~(bitMask);
+
+        int newNumber =  notBitMask & n ;
+        System.out.println(newNumber);
+
+
+
+    }
+}
