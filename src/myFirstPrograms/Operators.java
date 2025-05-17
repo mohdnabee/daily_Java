30edf77070fcd8b3dae6758a4b125d6a6527e851
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-17 19:35:36 +0530

    opertaors

---
+++ src/myFirstPrograms/Operators.java
@@ -0,0 +1,26 @@
+package myFirstPrograms;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int a =  10  , b =  5 ;
+
+        System.out.println("Operators Java :-");
+
+        System.out.println("\nArithmetic Operators:-");
+        System.out.println("  Binary Operators:- ");
+        System.out.println("  let A= 10 , B = 5");
+        System.out.println("  + => A+B "+ (a+b));
+        System.out.println("  - => A+B "+ (a-b));
+        System.out.println("  * => A+B "+ (a*b));
+        System.out.println("  / => A+B "+ (a/b));
+        System.out.println("  % => A+B "+ (a%b));
+
+        System.out.println("\nUnary Operators:- ");
+        System.out.println("1. ++ => Increment Operator");
+        System.out.println("2. -- => Decrement Operator");
+        System.out.println( "\n* Increment Operator \n  .Pre Increment Operator = ++a  \n  .post Increment Operator = a++");
+
+
+    }
+}
