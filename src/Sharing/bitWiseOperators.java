859320b71321799d12f37470730b03892a14f3a4
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-18 18:05:52 +0530

    bit wise Operators java

---
+++ src/Sharing/bitWiseOperators.java
@@ -0,0 +1,18 @@
+package Sharing;
+
+public class bitWiseOperators {
+    public  void bitWiseOper(){
+
+        System.out.println("Bitwise Operators");
+        int a = 5;
+        int b =  10;
+
+
+        System.out.println("operators");
+        System.out.println("  let a = 1010,  b = 1010");
+        int c = ( a & b) ;
+        System.out.println("& (binary And) =  a & b = "+ c);
+
+
+    }
+}
