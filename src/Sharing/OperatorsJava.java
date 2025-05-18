80eaafeff7c594bad8fcc6015bf69a818ae143e8
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-18 18:06:04 +0530

    operators Java

--- src/Sharing/OperatorsJava.java
+++ src/Sharing/OperatorsJava.java
@@ -1,10 +1,37 @@
 package Sharing;
 
+import java.util.Scanner;
+
 public class OperatorsJava {
     public static void main(String[] args) {
 
-        ArithmeticOperators  operator1=  new ArithmeticOperators();
-        operator1.binaryOperators();
+        System.out.println("1. Arithmetic Operators");
+        System.out.println("2. Relational Operators");
+        System.out.println("3. Logical Operators");
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Case Number : ");
+        int oper = sc.nextInt();
+        switch (oper){
+            case  1 :
+                ArithmeticOperators  operator=  new ArithmeticOperators();
+                operator.binaryOperators();
+                break;
+            case 2 :
+                RelationalOperators operators1 =  new RelationalOperators();
+                operators1.RelationOpe();
+                break;
+            case 3 :
+                logicalOpertaors opertaors2 = new logicalOpertaors();
+                opertaors2.logicalOpere();
+            case 4 :
+                bitWiseOperators operators3 = new bitWiseOperators();
+                operators3.bitWiseOper();
+                break;
+            default:
+                System.out.println(" Not Recorded  or Invalid Number");
+                break;
+        }
 
     }
 }
