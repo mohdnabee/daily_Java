0e2368e42528753de42a60e1ac1e2e15fac7d657
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-18 22:28:34 +0530

    bubble sort

--- src/SortingJava/BubbleSort.java
+++ src/SortingJava/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         int arr [] =  {7 , 8 , 3 , 1 ,2,9};
 
-        //  bubble sort
+        //  bubble sort    //  time complexity O (n ^ 2 )
         for (int i = 0; i<arr.length; i++ ){//  outer loop n-1 iteration counts kar ga
 //  n -1
             for (int j =0; j<arr.length-i-1; j++ ){
