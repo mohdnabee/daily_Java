e7cfb3f86a17fa120d395dc7ea48959a2aed36c7
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-19 19:48:44 +0530

    recursion 19-05-2025

---
+++ src/recursionJAVA/recursion1.java
@@ -0,0 +1,17 @@
+package recursionJAVA;
+/// Q.  print number from 5 to 1 :
+public class recursion1 {
+    public static void printNumber(int n ){
+
+        if (n ==0 ){
+            return;
+        }
+        System.out.print(n+ " ");
+        printNumber(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n =  5 ;
+        printNumber(n);
+    }
+}
