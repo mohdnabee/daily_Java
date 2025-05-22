94a6eb9caf838cd1023c6eda209a6324228f4ea3
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-22 13:03:36 +0530

    exception handling String

---
+++ src/ExceptionHAndling/userInput2.java
@@ -0,0 +1,19 @@
+package ExceptionHAndling;
+
+public class userInput2 {
+    public static void main(String[] args) {
+        String Str  =  null;
+
+        try {
+            System.out.println(Str.toUpperCase());
+        }catch (Exception e){
+            System.out.println(" Exception shows :-");
+            System.out.println(e);
+            System.out.println(" Your String  value is Null");
+        }
+
+        System.out.println(" example ");
+
+
+    }
+}
