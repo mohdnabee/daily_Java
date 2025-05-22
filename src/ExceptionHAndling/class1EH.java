555d81f80c68c161598b424662421a0b74601d8c
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-22 12:41:53 +0530

    exception handling

---
+++ src/ExceptionHAndling/class1EH.java
@@ -0,0 +1,19 @@
+package ExceptionHAndling;
+
+public class class1EH {
+    public static void main(String[] args) {
+         int a = 10 ,  b=  0 ;
+
+         try {
+             int c =  a / b ;
+             System.out.println(c);
+         }
+         catch (Exception e){
+             System.out.println(a +  " / "+ b +  " (we can not divide by zero) ");
+         }
+
+        System.out.print(" This is Example  of ");
+        System.out.print( "Exception Handling");
+    }
+}
+
