32d202c0498bcd938e1342e08bda0bf9ad7bf951
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-23 21:55:09 +0530

    recursion Java 23.05.25

--- src/recursionJAVA/recursSunSequenceJava.java
+++ src/recursionJAVA/recursSunSequenceJava.java
@@ -1,7 +1,26 @@
 package recursionJAVA;
 
 public class recursSunSequenceJava {
-    public static void main(String[] args) {
+    public  static void subSequences(String str ,  int idx ,  String newString){
+        if (idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
 
+        char currcahr =  str.charAt(idx);
+
+        //  to be choose
+        subSequences(str,idx+1,newString+currcahr);
+
+
+
+        //  or not to be
+        subSequences(str,idx+1,newString);
+
+    }
+
+    public static void main(String[] args) {
+        String str =  "abc";
+        subSequences(str,0,"");
     }
 }
