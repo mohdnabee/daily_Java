b87e35b05109bdfb20b3c06627a3055862cd2f63
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-27 20:37:26 +0530

    recursion titles place  27.05.25

---
+++ src/recursionJAVA/titles.java
@@ -0,0 +1,32 @@
+package recursionJAVA;
+
+public class titles {
+
+    public  static  int placeTitles(int n ,  int m){
+//  base case
+        if ( n ==  m ){
+            return  2;
+        }
+
+        if (n < m){
+            return 1 ;
+        }
+
+        //  vertical
+        int vertical =  placeTitles(n-m, m);
+        //  horizontals
+        int horizontals =  placeTitles(n-1,m);
+
+        return  vertical + horizontals  ;
+
+    }
+
+    public static void main(String[] args) {
+
+        int m  =  2 ,  n =4 ;
+        int totalTiles =  placeTitles(n,m);
+
+        System.out.println(totalTiles);
+
+    }
+}
