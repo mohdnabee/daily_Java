c2317d234ee75d97efcaf0d1d6ce9b72e3b2bc6e
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-28 12:30:09 +0530

    sets using java 28/05/25

---
+++ src/sets/class28_05_1.java
@@ -0,0 +1,10 @@
+package sets;
+
+public class class28_05_1 {
+    public static void main(String[] args) {
+
+        //  sets are store unique value
+        //  sets can not be repeats same elements
+
+    }
+}
