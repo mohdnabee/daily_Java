74dc5285363c572dd207f36b1d372102f9145370
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-28 12:38:54 +0530

    using  hashSets java 28/05/25

--- src/sets/class28_05_1.java
+++ src/sets/class28_05_1.java
@@ -1,5 +1,7 @@
 package sets;
 
+import java.util.HashSet;
+
 public class class28_05_1 {
     public static void main(String[] args) {
 
@@ -7,6 +9,13 @@ public class class28_05_1 {
         //  sets can not be repeats same elements
 
 
+        int [] aar =  {3 , 21,  34 ,  4 , 4 , 56 ,  6 , 6 , 4};
 
+        HashSet<Integer> set =  new HashSet<>();
+        for (int i  :  aar){
+            set.add(i);
+        }
+
+        System.out.println(set);
     }
 }
