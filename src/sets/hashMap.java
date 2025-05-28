0accb8b35e49593dc4676e9054e362ce357fb5e8
Mohdnabee <mohdnabeel989@gmail.com>
2025-05-28 12:45:47 +0530

    using  hashMap java 28/05/25

---
+++ src/sets/hashMap.java
@@ -0,0 +1,24 @@
+package sets;
+
+import java.util.HashMap;
+
+public class hashMap {
+    public static void main(String[] args) {
+
+
+        HashMap<String, Integer> map =  new HashMap<>();
+        map.put("id0", 1);// key and value ||  key always unique
+        map.put("id1", 2);
+        map.put("id2", 3);
+        map.put("id3", 4);
+        System.out.println(map);
+
+
+        System.out.println(map.get("id0"));
+        System.out.println(map.getOrDefault("id0",0));
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+
+
+    }
+}
