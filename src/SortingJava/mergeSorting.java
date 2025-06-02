5ee2b53132ee8d097496698ec7244163b8653bd8
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-02 19:24:25 +0530

    merge sort java 02/06/25

--- src/SortingJava/mergeSorting.java
+++ src/SortingJava/mergeSorting.java
@@ -1,11 +1,14 @@
 package SortingJava;
 
 public class mergeSorting {
+
+
     public static  void conquer (int arr[] , int si ,  int mid , int ei){
         int merged [] =  new int[ei -si+1];
         int idx1=  si;
         int idx2 =  mid +1;
         int x =  0;
+
 
         while (idx1 <= mid && idx2 <= ei){
             if(arr[idx1] <= arr[idx2]){
@@ -24,7 +27,11 @@ public class mergeSorting {
         for (int i =0,  j=si; i<merged.length; i++, j++){
             arr[j] =  merged[i];
         }
+
+
     }
+
+
     public  static  void  divide(int arr[], int si , int ei){
         if (si >=  ei){
             return;
@@ -34,7 +41,10 @@ public class mergeSorting {
         divide(arr, si, mid );
         divide(arr , mid+1 , ei);
         conquer(arr, si, mid, ei);
+
+
     }
+
     public static void main(String[] args) {
 
         int arr []=  {6,3,9,5,2,8};
