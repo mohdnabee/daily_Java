26987cccbf6feb78be7d21f7db4c87bceb56153d
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-02 23:27:42 +0530

    merge sort java 02/06/25

--- src/SortingJava/mergeSorting.java
+++ src/SortingJava/mergeSorting.java
@@ -47,7 +47,7 @@ public class mergeSorting {
 
     public static void main(String[] args) {
 
-        int arr []=  {9,8,7,6,5,4,3,2,1};
+        int arr []=  {9,8,7,6,5,4,3,2,1, 100 , 101, 80 , 50 };
         int n =  arr.length;
 
         divide(arr,0,n-1);
