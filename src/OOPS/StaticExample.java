52acd4f8a6e4731ef54fcf1e2b2e74eeb53208f3
Mohdnabee <mohdnabeel989@gmail.com>
2025-06-10 22:12:30 +0530

    oops static keyword

---
+++ src/OOPS/StaticExample.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+class Studentss{
+    String name ;
+     static String school ;//  common for all objects
+
+    public static void changeSchool(){
+        school =  "NewSchool";
+    }
+}
+
+
+
+public class StaticExample {
+    public static void main(String[] args) {
+      Studentss.school= "GE";
+      Studentss student1 =  new Studentss();
+      student1.name="tony";
+        System.out.println(student1.school);
+    }
+}
