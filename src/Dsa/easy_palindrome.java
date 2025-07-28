334fb55ae67644431ad3f6e2ddb097c665b0aa4a
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-28 20:52:19 +0530

    easy  dsa question

---
+++ src/Dsa/easy_palindrome.java
@@ -0,0 +1,21 @@
+package Dsa;
+
+public class easy_palindrome {
+
+    public  static  void  main  (String[] args){
+        String str  =  "madam";
+        String rev  =  "";
+
+        for (int i  =  str.length()-1; i>=0; i--){
+            rev +=  str.charAt(i);
+        }
+
+        if (str.equals(rev)){
+            System.out.println("Palindrome");
+
+        }else {
+            System.out.println("Not Palindrome");
+        }
+
+    }
+}
