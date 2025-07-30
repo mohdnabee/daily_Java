6bac575c976bb1bc87ea61c568083192b4386241
Mohdnabee <mohdnabeel989@gmail.com>
2025-07-30 23:36:39 +0530

    easy  dsa question

--- src/Dsa/easy_SumOFAllEelemnts.java
+++ src/Dsa/easy_SumOFAllEelemnts.java
@@ -5,11 +5,18 @@ public class easy_SumOFAllEelemnts {
 
         int [] arr = {5,10,20,15};
         int sum  =0 ;
+        int div =  0 ;
 
         for (int num : arr){
             sum +=  num;
         }
 
+
+        for  (int num :arr){
+            div =  sum / 2 ;
+        }
+
         System.out.println("Sum :"+ sum);
+        System.out.println("Div:" + div);
     }
 }
